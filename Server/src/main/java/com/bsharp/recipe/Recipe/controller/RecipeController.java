c158c58bbc7ba32249887e4f693bfa34579053b8
Gary Tin <tinkwaiki@gmail.com>
2024-10-30 17:51:50 +0800

    feat: add endpoint for recipe

--- Server/src/main/java/com/bsharp/recipe/Recipe/controller/RecipeController.java
+++ Server/src/main/java/com/bsharp/recipe/Recipe/controller/RecipeController.java
@@ -1,20 +1,25 @@
 package com.bsharp.recipe.Recipe.controller;
 
-import com.bsharp.recipe.Recipe.dto.request.AddIngredientRequest;
-import com.bsharp.recipe.Recipe.dto.request.IngredientQueryParmas;
-import com.bsharp.recipe.Recipe.entity.IngredientEntity;
-import com.bsharp.recipe.Recipe.service.IngredientService;
-import jakarta.validation.Valid;
+import com.bsharp.recipe.Recipe.dto.request.CreateRecipeRequest;
+import com.bsharp.recipe.Recipe.entity.RecipeEntity;
+import com.bsharp.recipe.Recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @PostMapping("/recipe")
+    public RecipeEntity createRecipe(@RequestBody CreateRecipeRequest request) {
+        return recipeService.createRecipe(request);
+    }
 
 }
